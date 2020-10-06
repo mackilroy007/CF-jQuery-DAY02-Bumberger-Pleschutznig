e43ac468935a93f6cb31603f0060bd323acdc0cf
mackilroy007 <mackilroy007@gmail.com>
2020-10-06 11:18:30 +0200

    finished basic and created intermediate work folder

--- basic/part 2/javaScript.js
+++ basic/part 2/javaScript.js
@@ -1,7 +1,7 @@
 //basic exercise
 //creating 3 empty img elements
 $(document).ready(function() {
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 9; i++) {
     var temp = $(`<div id="img${i}"><img src=""></div>`);
     $("main").append(temp.clone());
 
@@ -12,24 +12,22 @@ for (let i = 0; i < 3; i++) {
 
 //allocating img src to the img elements
 $(document).ready(function() {
-        $("#img0 img").attr({
+        $("img:odd").attr({
             "src": "https://cdn.shopify.com/s/files/1/1369/2605/articles/White_Gift_Box_with_Red_Bow_PNG_Image_2048x.png?v=1511752878",
             "value": "Gift Box"
     });         
-        $("#img1 img").attr({
+        $("img:even").attr({
             "src": "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
-            "value":"Santa Claus"
-    });
-        $("#img2 img").attr({
-            "src": "https://freepngimg.com/download/christmas_bell/1-2-christmas-bell-png-image.png",
-            "value":"Bells"
+            "value":"Santa Claus",
     });
 });
 
-// //click hearing function displays value of img
+// //click hearing function hide img
 $(document).ready(function() {
     $('img').on('click', function() {
-        var pic = $(this).attr("value");
-        $("#textbox").text(`You clicked ${pic}`);
+        //var pic = $(this).attr("value");
+        if($(this).attr("value") == "Santa Claus"){
+            $(this).hide();
+        }
      })
 });
