63929f429e8d48abba88405a959f1a98b50c46f0
mackilroy007 <mackilroy007@gmail.com>
2020-10-06 13:41:58 +0200

    fixed json display error

--- intermediate/javaScript.js
+++ intermediate/javaScript.js
@@ -1,14 +1,10 @@
 
-
-
-
-
-
 //$(document).ready(function(){
     var slides = JSON.parse(fotos);
+    var swiper = document.getElementsByClassName("swiper-wrapper")[0];      //swiper-wrapper is an array even if it is
+    for (var i = 0; i < slides.length; i++){
+        console.log(swiper);
 
-    for (var i = 0; i < slides.lenght; i++){
-        console.log("heay");
-        document.getElementsByClassName("test").innerHTML += `<div class="swiper-slide"><img scr=${slides[i].foto} alt=${slides[i].info}></div>`;
+        swiper.innerHTML += `<div class="swiper-slide"><img src=${slides[i].foto} alt=${slides[i].info}></div>`;
     };
 //});
