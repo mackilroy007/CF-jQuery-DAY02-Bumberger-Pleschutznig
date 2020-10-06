48bc8399d2675bf0105cf7b1baeaebfc238c223a
mackilroy007 <mackilroy007@gmail.com>
2020-10-06 15:50:06 +0200

    final touches

--- intermediate/javaScript.js
+++ intermediate/javaScript.js
@@ -1,5 +1,6 @@
 //pulling json content into for loop and inserting it into the slide library
 var slides = JSON.parse(fotos);
+
 var swiper = document.getElementsByClassName("swiper-wrapper")[0]; //swiper-wrapper is an array even if it is
 for (var i = 0; i < slides.length; i++) {
     console.log(swiper);
